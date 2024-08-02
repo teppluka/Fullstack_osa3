c56fe8ec0578f567bdae5ba79ea0e739b639f231
Luka Tepponen <anna.tepponen@helsinki.fi>
2024-08-02 15:08:51 +0300

    osa 3

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express()
 
 app.use(express.json())
 
+app.use(express.static('dist'))
+
 app.use(morgan(function (tokens, req, res) {
     if (tokens.method(req, res) !== "POST") {
         return [
